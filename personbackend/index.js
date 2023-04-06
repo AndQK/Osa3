ebc44d2fd56d01c345c6a066aa99b8f585852dbf
Antonio <antonio.skondras@gmail.com>
2023-04-06 21:57:33 +0300

    added cors

--- personbackend/index.js
+++ personbackend/index.js
@@ -2,8 +2,9 @@
 const express = require('express')
 const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
-
+app.use(cors())
 app.use(express.json())
 
 morgan.token('body', (req, res) => {
