694e21f7bf57b85e14c25b81588d34892551b91b
Antonio <antonio.skondras@gmail.com>
2023-04-06 22:29:16 +0300

    small changes

--- personbackend/index.js
+++ personbackend/index.js
@@ -16,6 +16,7 @@ morgan.token('body', (req, res) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
+app.use(express.static('build'))
 
 
 let persons = [
