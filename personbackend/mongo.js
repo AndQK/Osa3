ad0a641b39c145515ba15089472badf6200637e6
Antonio <antonio.skondras@gmail.com>
2023-05-21 21:13:03 +0300

    tehtävä3.22 added eslint for syntax correction

--- personbackend/mongo.js
+++ personbackend/mongo.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-    console.log('give password as argument')
-    process.exit(1)
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 const password = process.argv[2]
@@ -15,38 +15,38 @@ mongoose.connect(url)
 
 
 const personSchema = new mongoose.Schema({
-    name: String,
-    number: String,
+  name: String,
+  number: String,
 })
 
-const Person = mongoose.model("Person", personSchema)
+const Person = mongoose.model('Person', personSchema)
 
 const printPerson = (person) => {
-    console.log(person.name, person.number)
+  console.log(person.name, person.number)
 }
 
 if (process.argv.length === 5) {
-    const person = new Person({
-        name: process.argv[3],
-        number: process.argv[4],
-    })
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+  })
 
-    person.save().then(result => {
-        console.log('added', person.name, 'number', person.number, 'to phonebook')
-        mongoose.connection.close()
-    })
-} else if (process.argv.length === 3) {
-    Person.find({})
-
-        .then(result => {
-            console.log('phonebook:')
-            result.forEach(person => {
-                printPerson(person)
-            })
-            mongoose.connection.close()
-        })
-} else {
-    console.log('you gave incorrect amount of arguments')
+  person.save().then(result => {
+    console.log('added', person.name, 'number', person.number, 'to phonebook')
     mongoose.connection.close()
-    process.exit(1)
+  })
+} else if (process.argv.length === 3) {
+  Person.find({})
+
+    .then(result => {
+      console.log('phonebook:')
+      result.forEach(person => {
+        printPerson(person)
+      })
+      mongoose.connection.close()
+    })
+} else {
+  console.log('you gave incorrect amount of arguments')
+  mongoose.connection.close()
+  process.exit(1)
 }
