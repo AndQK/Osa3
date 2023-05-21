5e70b8c60af99f16aa11c33579564073b0e8462e
Antonio <antonio.skondras@gmail.com>
2023-05-21 12:58:45 +0300

    tehtävä 3.12

---
+++ personbackend/mongo.js
@@ -0,0 +1,51 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://antonioskondras:${password}@cluster0.czq10v7.mongodb.net/personApp?retryWrites=true&w=majority`
+
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
+
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model("Person", personSchema)
+
+const printPerson = (person) => {
+    console.log(person.name, person.number)
+}
+
+if (process.argv.length === 5) {
+    const person = new Person({
+        name: process.argv[3],
+        number: process.argv[4],
+    })
+
+    person.save().then(result => {
+        console.log('person saved!')
+        mongoose.connection.close()
+    })
+} else if (process.argv.length === 3) {
+    Person.find({})
+             
+        .then(result => {
+            console.log('phonebook:')
+            result.forEach(person => {
+                printPerson(person)
+            })
+            mongoose.connection.close()
+        })
+} else {
+    console.log('you gave incorrect amount of arguments')
+    process.exit(1)
+}
